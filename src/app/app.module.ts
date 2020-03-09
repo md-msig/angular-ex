e683a706ecf5513661c89a3dc5aef17248080c16
md-msig <mdmsig20@gmail.com>
2020-03-10 00:06:55 +0800

    create home&contactus page and change route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { ChangeTextDirective } from './change-text.directive';
@@ -15,7 +15,8 @@ import { SqrtPipe } from './app.sqrt';
     NewCmpComponent,
     ChangeTextDirective,
     PipeTestComponent,
-    SqrtPipe
+    SqrtPipe,
+    RoutingComponent
   ],
   imports: [
     BrowserModule,
